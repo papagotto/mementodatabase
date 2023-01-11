3733b191ed209bb047bcec6ed28a4ac4b9374811
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-11 16:17:17 -0300

    Create loguearV.js

---
+++ loguearV.js
@@ -0,0 +1,13 @@
+function logVar(a){
+    
+    libreria=libByName("Log");
+        
+    nuevoLog= new Object();
+    
+    nuevoLog["Libreria"]= l.title
+    nuevoLog["Asunto"]="Return";
+    nuevoLog["Descripcion"]=a;
+    nuevoLog["Fecha y Hora"] = new Date();
+
+    libreria.create(nuevoLog);
+}
