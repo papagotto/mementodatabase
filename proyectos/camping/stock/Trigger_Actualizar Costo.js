b1acdd4a96fba2c1867d252df70422794a97d753
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-11 18:35:47 -0300

    optimizacion de coidgo

--- proyectos/camping/stock/Trigger_Actualizar Costo.js
+++ proyectos/camping/stock/Trigger_Actualizar Costo.js
@@ -1,26 +1,18 @@
 function actualizarCosto(e){
-try{
+   
 
   
-var precios= e.linksFrom("Precios","Producto")
+   var precios= e.linksFrom("Precios","Producto")
 
-for(var i in precios){
+   for(var i in precios){
     
-    if(precios[i].field("Actual")== true){ 
+      if(precios[i].field("Actual")== true){ 
             
-       var registroActualizado= precios[i] ;
        var precioActualizado= precios[i].field("Monto"); 
     
-    }
+       }
 
-}
-e.set("Costo",precioActualizado);
+   }
+   e.set("Costo",precioActualizado);
  
-
-}catch(Err){
-
-   loguear(Err);    
-    
-
-}
 }
