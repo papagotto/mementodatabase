9540f2d35d2e2b2794970091317fca719476e16a
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-11 14:46:11 -0300

    Create loguear.js

---
+++ loguear.js
@@ -0,0 +1,13 @@
+function loguear(a){
+    let lib = lib();
+    libreria=libByName("Log");
+        
+    nuevoLog= new Object();
+    
+    nuevoLog["Libreria"]= lib.name
+    nuevoLog["Asunto"]=a["name"];
+    nuevoLog["Descripcion"]=a["message"];
+    nuevoLog["Fecha y Hora"] = new Date();
+
+    libreria.create(nuevoLog);
+}
