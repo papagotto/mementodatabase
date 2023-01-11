5c915e23102fe0d540f6a32328e253c579e367a3
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-11 17:11:31 -0300

    Create Trigger_Actualizar Costo.js

---
+++ proyectos/camping/stock/Trigger_Actualizar Costo.js
@@ -0,0 +1,24 @@
+try{
+
+var precios= e.linksFrom("Precios","Producto")
+
+ 
+for(var i in precios){
+    
+    if(precios[i].field("Actual")== true){ 
+            
+       var registroActualizado= precios[i] ;
+       var precioActualizado= precios[i].field("Monto"); 
+    
+    }
+
+}
+e.set("Costo",precioActualizado);
+ 
+
+}catch(Err){
+
+   loguear(Err);    
+    
+
+}
