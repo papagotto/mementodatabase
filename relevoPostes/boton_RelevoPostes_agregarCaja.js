0e12cb293b5adf8979db30cb1fe552b6b5da9f84
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-18 16:16:16 -0300

    Update boton_RelevoPostes_agregarCaja.js

--- relevoPostes/boton_RelevoPostes_agregarCaja.js
+++ relevoPostes/boton_RelevoPostes_agregarCaja.js
@@ -1,12 +1,13 @@
-function agregarCaja(e){
+function agregarCaja(e,arg){
 
     var ubicacion = e.field("Ubicacion");
     var libreria = libByName("Relevo Cajas");
     var entradaNueva = new Object;
     
+    
     entradaNueva["Ubicacion"] = ubicacion; 
     entradaNueva["Id Poste"] = e.field("Numero");
-    
+    entradaNueva["Tipo Caja"]= arg;
 
     libreria.create(entradaNueva);
 
