5cd17b597da5e9a9efa98e3edb1b62412a7e5336
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-20 17:42:43 -0300

    Update logVar.js

--- funciones/logVar.js
+++ funciones/logVar.js
@@ -7,7 +7,7 @@ function logVar(a,l){
     nuevoLog["Libreria"]= l.title
     nuevoLog["Asunto"]="Return";
     nuevoLog["Descripcion"]= a;
-    nuevoLog["Fecha y Hora"] = new Date().toLocaleString();
+    nuevoLog["Fecha y Hora"] = moment().format('DD/MM/YYYY hh:mm')
 
     libreria.create(nuevoLog);
 }
