271e43b0c8dd08e3caaced0d2a28a76f47551562
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-23 10:33:07 -0300

    algo

--- funciones/logs.js
+++ funciones/logs.js
@@ -1,3 +1,17 @@
+function logVar(a){
+	
+	var libreria=libByName("Log");
+
+    var nuevoLog= new Object();
+    
+    nuevoLog["Libreria"]= l.title
+    nuevoLog["Asunto"]="Return";
+    nuevoLog["Descripcion"]= a;
+   
+
+    libreria.create(nuevoLog);
+}
+
 function logErr(a){
     
     libreria=libByName("Log");
